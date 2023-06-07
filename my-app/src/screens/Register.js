4e4acbabb9e0ffcaacb519fbaf6a5939bb79704f
Brucebe <bebert@udesa.edu.ar>
2023-06-07 00:43:42 -0300

    Home + nuevas rutas

--- my-app/src/screens/Register.js
+++ my-app/src/screens/Register.js
@@ -1,16 +1,41 @@
-import { Text, View } from 'react-native'
+import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { Component } from 'react'
 import RegisterForm from '../components/RegisterForm'
+import { auth } from '../firebase/config'
+import { withSafeAreaInsets } from 'react-native-safe-area-context'
 
 class Register extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            props: props,
+            email: '',
+            password:'',
+
+        }
+    }
+
+    componentDidMount(){
+        auth.onAuthStateChanged(user => {
+          if(user){
+            this.props.navigation.navigate('Login')
+          }
+        })
+      }
+
   render() {
     return (
-      <View>
-        <Text>Registrate aqui</Text>
-        <RegisterForm/>
-      </View>
+ <View>
+
+        <RegisterForm navigation={this.props.navigation}/>
+ </View>
+
+
+      
     )
   }
 }
+
+
 
 export default Register
